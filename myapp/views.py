c8d456ae4995d8fccf7784ca50a23f90fbe33486
mahabubsheikhriyad <130032178+mahabubsheikhriyad@users.noreply.github.com>
2024-06-28 22:27:29 +0600

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import jwt
 
-@login_required
 def index(request):
     j = jwt.__version__
     return render(request, 'index.html', {'user': request.user, 'j': j})
